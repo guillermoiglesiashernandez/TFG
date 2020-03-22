19f8125ef48eddeab4e72274ad2df8236ce8ec40
guillermoiglesiashernandez <guillermoiglesiashernandez@alumnos.upm.es>
2020-03-22 14:55:18 +0100

    Added machine learning training

--- CNN/venv/cnn_DatasetFijo.py
+++ CNN/venv/cnn_DatasetFijo.py
@@ -6,7 +6,7 @@ import tensorflow
 import seaborn as sns
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Activation
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras import backend as K
 from PIL import Image
